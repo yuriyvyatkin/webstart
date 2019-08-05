9c4ef25a85cd83707c1e90e3a178194a8d6036cd
Yuriy <yuriyvyatkin@ya.ru>
2019-08-05 22:45:53 +1000

    Добавлено автозакрытие модального окна с помощью JS.

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,13 @@ var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
 
+var remove = function() {
+	modal.classList.remove('modal_active');
+}
+
 button.addEventListener('click', function() {
 	modal.classList.add('modal_active');
+	setTimeout(remove, 5000)
 });
 
-close.addEventListener('click', function() {
-	modal.classList.remove('modal_active');
-});
+close.addEventListener('click', remove);
