71852de07b59debb166d80a601dede03420067e4
Yuriy <yuriyvyatkin@ya.ru>
2019-08-10 16:18:27 +1000

    Выполнено домашнее задание 21 (простое)

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,22 +1,34 @@
-// Модальное окно с JS
+//подключение библиотеки WOW
+new WOW().init()
+
+// Модальное окно с JS для кнопки Перезвоните мне
 $(document).ready(function(){
   var button = $('#button');
   var modal = $('#modal');
   var close = $('#close');
-  var cardLink = $('.card__link');
   
   button.on('click', function() {
     modal.addClass('modal_active');
   });
 
-  // cardLink.on('click', function(event) {
-  //   event.preventDefault();
-  //   modal.addClass('modal_active');
-  // });
-
   close.on('click', function() {
     modal.removeClass('modal_active');
   })
+});
+
+// AJAX запросы к модальному окну
+$('#callback-form').on('submit', function(event) {
+  event.preventDefault();
+  $.ajax({
+    url: 'php/mail.php',
+    type: 'POST', 
+    data: $(this).serialize(),
+    success: function(data) {
+      $('.success').text(data + ', Ваша форма отправлена!');
+      alert("Спасибо за заявку, скоро мы вам перезвоним.");
+      $('.input').val('');
+    } 
+  });
 });
 
 // Модальное окно с JQuery
@@ -50,7 +62,7 @@ $(document).ready(function() {
   });
 });
 
-// AJAX запросы
+// AJAX запросы к блоку offer
 // $('#offer-form').on('submit', function(event) {
 //   event.preventDefault();
 //   $.ajax({
@@ -65,17 +77,56 @@ $(document).ready(function() {
 //   });
 // });
 
-// AJAX запросы к форме callback-form
-$('#callback-form').on('submit', function(event) {
-  event.preventDefault();
-  $.ajax({
-    url: 'php/mail.php',
-    type: 'POST', 
-    data: $(this).serialize(),
-    success: function(data) {
-      $('.success').text(data + ', Ваша форма отправлена!');
-      alert("Спасибо за заявку, скоро мы вам перезвоним.");
-      $('.input').val('');
-    } 
+// слайдер при помощи slick
+$(document).ready(function() {
+  $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.arrows__left'),
+    nextArrow: $('.arrows__right'),
+    responsive: [
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          centerMode: true,
+          variableWidth: true,
+          autoHeight:true,
+          autoHeightClass: 'owl-carousel',
+          responsiveBaseWidth: '#owl-example',
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
+
+//слайдер при помощи owl-carousel
+// $(document).ready(function(){
+//   $(".owl-carousel").owlCarousel({
+//     loop: true,
+//     margin: 30,
+//     nav: true,
+//     navText: ["<img src='img/portfolio/left-arrow.png'>", "<img src='img/portfolio/right-arrow.png'>"],
+//     responsive:{
+//         0:{
+//           autoWidth:true,
+//           center: true,
+//           items: 1
+//         },
+//         770:{
+//           items: 2
+//         },
+//         1201:{
+//           items: 3
+//         }
+//     }
+//   });
+// });
